c39eafb1868f429731dbf7777451bae04c09b97d
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-23 05:37:14 -0700

    First commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+//  The function should print a sentence summarizing the size of the shirt and the message printed on it.
+//  Call the function.
+
+function make_shirt(size: string, text: string){
+    console.log(`Creating a ${size} sizes shirt with a ${text} text message printed on it.`);
+    
+}
+
+make_shirt("small","'Free platestine'");
